51977b8326332f5f711ac5ae209e8d08b17df5e1
memosky3 <memo_inge@hotmail.com>
2023-02-19 23:37:03 -0600

    all project
    all project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,14 +1,19 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC-lHJZR3Gqxm24_Vd_AJ5Yw&part=snippet%2Cid&order=date&maxResults=9';
 
-const content = null || document.getElementById('content');
+const content = null || document.getElementById('content') ;
 
 const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-    'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '44ccaf15b8msh21c2c9c8c4a7d06p14c3c6jsn985851d42b05',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
 };
+/*
+fetch('https://youtube-v31.p.rapidapi.com/search?channelId=UC-lHJZR3Gqxm24_Vd_AJ5Yw&part=snippet%2Cid&order=date&maxResults=9', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));*/
 
 async function fetchData(urlApi) {
   const response = await fetch(urlApi, options);
@@ -24,7 +29,7 @@ async function fetchData(urlApi) {
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
@@ -33,10 +38,12 @@ async function fetchData(urlApi) {
           </h3>
         </div>
       </div>
-    `).slice(0,4).join('')}
+    `).slice(0,4).join('')}   //slice 0,4 regresa solo los primeros 4 elementos del array
 
     `;
-  } catch {
-
+    content.innerHTML = view;
+  } catch (error){
+    console.log(error);
+    
   }
-})();
+})(); //se ejecuta funcion anonima
